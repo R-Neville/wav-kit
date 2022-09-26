ad0de621ca3784eaebd93d0c87eb08386ec5cd52
Robert Neville <robertneville27@gmail.com>
2022-09-26 19:40:55 +1000

    Add title and default path to open folder dialog

--- src/main/initDialogs.ts
+++ src/main/initDialogs.ts
@@ -1,3 +1,4 @@
+import os from "os";
 import { ipcMain, dialog, BrowserWindow } from "electron";
 
 export default function initDialogs(window: BrowserWindow) {
@@ -13,6 +14,8 @@ export default function initDialogs(window: BrowserWindow) {
 
   ipcMain.handle("dialog:open-folder", async () => {
     const outcome = await dialog.showOpenDialog(window, {
+      title: "Open Folder",
+      defaultPath: os.homedir(),
       properties: ["openDirectory"],
     });
     if (outcome.canceled || outcome.filePaths.length === 0) {
