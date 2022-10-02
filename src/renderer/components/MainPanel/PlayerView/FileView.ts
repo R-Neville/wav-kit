212917589662c6f86e2c8794752745da650866ba
Robert Neville <robertneville27@gmail.com>
2022-10-02 10:36:17 +1000

    Add 'no files' message and import button - displayed when no files have been imported

--- src/renderer/components/MainPanel/PlayerView/FileView.ts
+++ src/renderer/components/MainPanel/PlayerView/FileView.ts
@@ -9,6 +9,7 @@ class FileView extends HTMLElement {
   private _items: FileViewItem[];
   private _scrollView: ScrollView;
   private _contentWrapper: HTMLDivElement;
+  private _noFiles: HTMLDivElement;
 
   constructor() {
     super();
@@ -17,10 +18,12 @@ class FileView extends HTMLElement {
     this._items = [];
     this._scrollView = new ScrollView("100%", "100%");
     this._contentWrapper = this.buildContentWrapper();
+    this._noFiles = this.buildNoFiles();
     this._scrollView.addVerticalScrollBar(this._contentWrapper, 10);
     this._scrollView.setContent(this._contentWrapper);
 
     this.appendChild(this._scrollView);
+    this._contentWrapper.appendChild(this._noFiles);
 
     applyStyles(this, {
       ...universalStyles,
@@ -49,6 +52,9 @@ class FileView extends HTMLElement {
   }
 
   addItem(stats: FileStats) {
+    if (this._items.length === 0) {
+      this._noFiles.remove();
+    }
     const item = new FileViewItem(stats);
     this._items.push(item);
     this._contentWrapper.appendChild(item);
@@ -59,6 +65,7 @@ class FileView extends HTMLElement {
       item.remove();
     });
     this._items = [];
+    this._contentWrapper.appendChild(this._noFiles);
     const customEvent = new CustomEvent("all-files-cleared", {
       bubbles: true,
     });
@@ -76,12 +83,50 @@ class FileView extends HTMLElement {
     return wrapper;
   }
 
+  private buildNoFiles() {
+    const noFiles = document.createElement("div");
+    noFiles.textContent = "You haven't imported any files yet.";
+    applyStyles(noFiles, {
+      ...universalStyles,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: "1em",
+      color: window.theme.fgHighlight,
+    } as CSSStyleDeclaration);
+    const button = document.createElement("button");
+    button.textContent = "Import";
+    applyStyles(button, {
+      ...universalStyles,
+      padding: "0.5em 1em",
+      border: "none",
+      borderRadius: "3px",
+      outline: "none",
+      marginTop: "1em",
+      backgroundColor: window.theme.bgAccent,
+      fontSize: "1em",
+      color: window.theme.fgAccent,
+      cursor: "pointer",
+    } as CSSStyleDeclaration);
+    button.addEventListener("click", () => {
+      const customEvent = new CustomEvent("import-file-requested", {
+        bubbles: true,
+      });
+      this.dispatchEvent(customEvent);
+    });
+    noFiles.appendChild(button);
+    return noFiles;
+  }
+
   private onClearItem(event: CustomEvent) {
     event.stopPropagation();
     const item = event.target as FileViewItem;
     const index = this._items.indexOf(item);
     item.remove();
     this._items.splice(index, 1);
+    if (this._items.length === 0) {
+      this._contentWrapper.appendChild(this._noFiles);
+    }
     const customEvent = new CustomEvent("file-cleared", {
       bubbles: true,
       detail: {
